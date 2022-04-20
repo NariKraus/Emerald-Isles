a80b0f25e968d581396337217b014df8e7c2c27f
NariKraus <99689678+NariKraus@users.noreply.github.com>
2022-04-21 00:22:19 +0100

    PanZoom Changes

--- assets/js/gallery-image.js
+++ assets/js/gallery-image.js
@@ -3,17 +3,6 @@ const previews = document.querySelectorAll(".gallery img, .gallery a, .stat-bloc
 const original = document.querySelector(".full-img");
 const caption = document.querySelector(".caption");
 
-function disableScrolling() {
-    var x = window.scrollX;
-    var y = window.scrollY;
-    window.onscroll = function() {
-        window.scrollTo(x, y);
-    };
-};
-
-function enableScrolling() {
-    window.onscroll = function() {};
-};
 previews.forEach(preview => {
     preview.addEventListener('click', () => {
         modal.classList.add('open');
@@ -37,69 +26,6 @@ modal.addEventListener('click', (e) => {
         closeFullscreen();
     };
 });
-const magnifying_area = document.getElementById("magnifying_area");
-const magnifying_img = document.getElementById("magnifying_img");
-var scrollCount = 1;
-magnifying_area.addEventListener('wheel', function(event) {
-    if (event.deltaY < 0 && scrollCount < 8) {
-        scrollCount++;
-    } else if (event.deltaY > 0 && scrollCount > 1) {
-        scrollCount--;
-    };
-    zoomWidth = 800 * (scrollCount / 8);
-    zoomHeight = 800 * (scrollCount / 8);
-    console.log(scrollCount);
-    console.log(magnifying_img.width);
-    magnifying_img.style.width = zoomWidth + '%';
-    magnifying_img.style.height = zoomHeight + '%';
-});
-const Pannable = (EL) => {
-    const initial = {
-        x: 0,
-        y: 0
-    };
-    let isPan = false;
-    const getXY = ({
-        clientX,
-        clientY
-    }) => {
-        const bcr = EL.getBoundingClientRect();
-        return {
-            x: clientX - bcr.left,
-            y: clientY - bcr.top,
-        };
-    };
-    const panStart = (ev) => {
-        ev.preventDefault();
-        isPan = true;
-        const {
-            x,
-            y
-        } = getXY(ev);
-        initial.x = EL.scrollLeft + x;
-        initial.y = EL.scrollTop + y;
-    };
-    const panMove = (ev) => {
-        if (!isPan) return; // Do nothing
-        const {
-            x,
-            y
-        } = getXY(ev);
-        EL.scrollTo(initial.x - x, initial.y - y);
-    };
-    const panEnd = (ev) => {
-        isPan = false;
-    };
-    var x = window.scrollX;
-    var y = window.scrollY;
-    EL.onscroll = function() {
-        window.scrollTo(x, y);
-    };
-    EL.addEventListener("mousedown", panStart);
-    document.addEventListener("mousemove", panMove);
-    document.addEventListener("mouseup", panEnd);
-};
-document.querySelectorAll(".viewport").forEach(Pannable);
 
 function openFullscreen() {
     if (modal.requestFullscreen) {
