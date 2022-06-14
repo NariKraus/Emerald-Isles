7a1464e62a54c1aa67af8e98cc69d3346b55276c
NariKraus <99689678+NariKraus@users.noreply.github.com>
2022-06-14 23:14:41 +0100

    Update navbar-loader.js

--- assets/js/navbar-loader.js
+++ assets/js/navbar-loader.js
@@ -1,13 +1,12 @@
 $( "#header" ).load( "/Emerald-Isles/assets/html/navbar.html", function () {
-    let params = (new URL(document.location)).searchParams;
-    let DM = params.get('DM');
-    if (DM == 'true') {
+    if (~document.location.search.indexOf("GM")) {
         $('.nav-link:hidden').attr('hidden', false);
-        for (let i = 0; i < $('a[href]').length; i++) {
-            const x = $('a.nav-link[href]')[i];
-            $(x).attr("href", function(i, href) {
-                return href + '?DM=true';
-            });
-        };
+        $('a').each(function() {
+            var href = $(this).attr('href');
+            if (href) {
+                href += (href.match(/\?/) ? '&' : '?') + 'GM';
+                $(this).attr('href', href)
+            }
+        });
     };
 });
